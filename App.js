83557597212391712313f06f0f5e587751d204be
PedroHGoncalves1 <162513880+PedroHGoncalves1@users.noreply.github.com>
2024-03-06 21:39:35 -0300

    clone ewerton

--- App.js
+++ App.js
@@ -2,14 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native'; 
 
 import Mensagem from './src/ex_2'; 
-import nomes from './src/ex_2/listaNomes';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {
-        nomes.map((pessoa, pos) => <Mensagem nome={pessoa.nome} pos={pos} idade={pessoa.idade} />)
-      }     
+    <Mensagem />
       <StatusBar style="auto" />
     </View>
   );
--- src/ex_2/index.js
+++ src/ex_2/index.js
@@ -2,21 +2,16 @@ import { View, Text } from 'react-native';
 
 import styles from './styles';
 
-export default function Mensagem ({nome, idade, pos}) { 
+export default function Mensagem () { 
 
-    // const mai = sobrenome.toUpperCase();
+
 
     return(
         <View style={styles.container}>
             <Text style={styles.mensagem}>
-                ID: {pos + 1}
+                react-native
             </Text>
-            <Text style={styles.mensagem}>
-                Nome: {nome}
-            </Text>
-            <Text style={styles.mensagem}>
-                Idade: {idade}
-            </Text>
+            
         </View>
     );
 }
